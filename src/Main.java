f92a926cb449c374b554400d4dd2ea5a6b2a17ce
walshorafa <walshorafa@smail.ucas.edu.ps>
2023-03-25 11:45:12 +0300

    first updat

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        //السؤال الاول
+        System.out.println("      * ");
+        System.out.println("     * *");
+        System.out.println("    * * *");
+        System.out.println("   * * * *");
+        System.out.println("  * * * * *");
+        System.out.println(" * * * * * *");
+        System.out.println("* * * * * * *");
+        System.out.println("    |  |       ");
+        System.out.println("    |  |       ");
+        System.out.println("    |  |       ");
+        System.out.println("    |  |       ");
+
+
+        System.out.println("\n");
+
+
+        //السؤال الثاني
+        System.out.println("* * * * * * * * * *");
+        System.out.println("* * * *     * * * *");
+        System.out.println("* * *         * * *");
+        System.out.println("* *             * *");
+        System.out.println("*                 *");
+        System.out.println("* *             * *");
+        System.out.println("* * *         * * *");
+        System.out.println("* * * *     * * * *");
+        System.out.println("* * * * * * * * * *");
+
+
+
+        System.out.println("\n");
+
+
+
+
+        //السؤال الثالث
+        System.out.println("*  *  *  *  *");
+        System.out.println("*    ( )    *");
+        System.out.println("*    / \\    *");
+        System.out.println("*  *  *  *  *");
+
+
+    }
+}
